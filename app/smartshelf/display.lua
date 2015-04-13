b50cf1d853bcf7e88ab98f13eaaeda214f95def7
Naren Vasanad <naren.vasanad@berkeley.edu>
2015-04-12 23:16:10 -0700

    More modifications to display

--- app/smartshelf/display.lua
+++ app/smartshelf/display.lua
@@ -1,8 +1,6 @@
 require "cord"
 sh = require "stormsh"
 
-LCD = require "lcd"
-Button = require "button"
 require "svcd"
 
 newItem = -1
@@ -10,21 +8,15 @@ newItemW = -1
 
 shelves = {}
 
-btn1 = Button:new("D2")
 curInd = 0
 noShelf = 4
-arr = storm.array.create(noShelf, storm.array.UINT16)
-
-lcd = LCD:new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
-cord.new(function ()
-    lcd:init(2, 1)
-    lcd:writeString("SmartShelf")
-end)
 
 cord.new(function()
     cord.await(SVCD.init, "shelfclient")
+    print("inside the cord")
     SVCD.advert_received = function(pay, srcip, srcport)
         local adv = storm.mp.unpack(pay)
+        print("got payload", pay)
         for k,v in pairs(adv) do
             --These are the services
             if k == 0x5101 then
@@ -33,16 +25,24 @@ cord.new(function()
                     if vv == 0x6105 and k == 0x5101 then
                         -- This is a SmartShelf new item service
                         if strips[srcip] == nil then
-                            print ("Discovered SmartShelf: ", srcip)
-			        SVCD.subscribe(srcip,0x5101, 0x6105, function(msg)
-                                   local rec = storm.array.fromstr(msg) 
-                                   newItem = rec:get(1)
-                                   newItemW = rec:get(2)
-                                   print(newItem, newItemW)
-                                end)
-			        SVCD.subscribe(srcip,0x5101, 0x6106, function(msg)
-                                    arr = storm.array.fromstr(msg)
-                                end)
+                            print ("Discovered SmartShelf newitem: ", srcip)
+			    SVCD.subscribe(srcip,0x5101, 0x6105, function(msg)
+                               local rec = storm.array.fromstr(msg) 
+                               newItem = rec:get(1)
+                               newItemW = rec:get(2)
+                               print("Detected change ", newItem, newItemW)
+                            end)
+                        end
+                        shelves[srcip] = storm.os.now(storm.os.SHIFT_16)
+                    end
+                    if vv == 0x6106 and k == 0x5101 then
+                        -- This is a SmartShelf new item service
+                        if strips[srcip] == nil then
+                            print ("Discovered SmartShelf getWeights: ", srcip)
+			    SVCD.subscribe(srcip,0x5101, 0x6106, function(msg)
+                                arr = storm.array.fromstr(msg)
+                                print("Got weights", arr)
+                            end)
                         end
                         shelves[srcip] = storm.os.now(storm.os.SHIFT_16)
                     end
@@ -57,7 +57,7 @@ function locateItem(name)
     cord.new(function()
         for k, v in pairs(shelves) do
             local cmd = name
-            local stat = cord.await(SVCD.write, k, 0x5101, 0x6105, cmd, 300)
+            local stat = cord.await(SVCD.write, k, 0x5101, 0x6107, cmd, 300)
             if stat ~= SVCD.OK then
                 print "FAIL"
             else
@@ -68,24 +68,6 @@ function locateItem(name)
         end
     end)
 end
-
-btn1:whenever("RISING", function()
-    print("Go to next item on list")
-    cord.new(function()
-        lcd:writeString("                ")
-        lcd:setCursor(1, 0)
-        lcd:writeString("                ")
-        lcd:setCursor(0, 0)
-        lcd:writeString("Shelf "+curInd+": "+arr[curInd]);
-        if (arr[curInd] < 2100) then
-            lcd:setBackColor(255, 0, 0)
-        else
-            lcd:setBackColor(0, 255, 0)
-        end
-        cord.await(storm.os.invokeLater, 100*storm.os.MILLISECOND)
-    end)
-    curInd = (curInd + 1)%#arr
-end)
 
 sh.start()
 cord.enter_loop()
