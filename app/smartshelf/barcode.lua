caf92b96d87fa66ba3c4a4a224abc57317b66017
Naren Vasanad <naren.vasanad@berkeley.edu>
2015-05-01 12:08:41 -0700

    Modified barcode.lua to receive string from C implementation and push it into SMAP middleware

--- app/smartshelf/barcode.lua
+++ app/smartshelf/barcode.lua
@@ -1,35 +1,50 @@
 require "cord"
+shellip = "2001:470:66:3f9::2" --IP of middleware service
 
 storm.io.set_mode(storm.io.INPUT, storm.io.D2) -- CLK Pin
 storm.io.set_mode(storm.io.INPUT, storm.io.D3) -- DATA Pin
 
-cord.new(function()
-	--prev = storm.io.get(storm.io.D2)
-	while true do
-	   --print(prev)
-	   --print(storm.io.get(storm.io.D2))
-	   --[[if storm.io.get(storm.io.D2) == 0 and prev == 1 then
-	      print("Detected!")
-	      storm.n.barcode_sample()
-	   end]]--
-	--code = {}
-	--for i = 1,11*8 do
-	   cord.await(storm.io.watch_single, storm.io.FALLING, storm.io.D2)
-	   --storm.n.barcode_sample()
-	   storm.n.barcode_sample_chars()
-	   --print(storm.io.get(storm.io.D3))
-	   --code[i] = storm.io.get(storm.io.D3)
-	--end
-	--for i = 1,11*8 do
-	   --storm.n.barcode_sample()
-	   --print(storm.io.get(storm.io.D3))
-	   --print(code[i])
-	--end]]--
-	--print(code)
-	--prev = storm.io.get(storm.io.D2)
+sendsock = storm.net.udpsocket(1337, function() end)
+
+barcodeVal = "x"
+
+scanned = true
+
+cord.new(function ()
+    while true do
+	if scanned then
+		storm.io.watch_single(storm.io.FALLING, storm.io.D2, function()
+		    barcodeVal = storm.n.barcode_sample_chars()
+		    print(barcodeVal)
+		    scanned = true
+		end)
+		scanned = false
 	end
+        local t = {barcodeVal}
+        storm.net.sendto(sendsock, storm.mp.pack(t), shellip, 2199)
+	cord.await(storm.os.invokeLater, 5000 * storm.os.MILLISECOND)
+    end
 end)
 
 sh = require "stormsh"
 sh.start()
 cord.enter_loop()
+
+
+--[[
+require "cord"
+
+storm.io.set_mode(storm.io.INPUT, storm.io.D2) -- CLK Pin
+storm.io.set_mode(storm.io.INPUT, storm.io.D3) -- DATA Pin
+
+cord.new(function()
+	while true do
+	   cord.await(storm.io.watch_single, storm.io.FALLING, storm.io.D2)
+	   barcodeVal = storm.n.barcode_sample_chars()
+           print(barcodeVal)
+	end
+end)
+
+sh = require "stormsh"
+sh.start()
+cord.enter_loop()]]--
